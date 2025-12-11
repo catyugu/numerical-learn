2c23294db5e171d5992c5d747dbdb5c6aee5794f
catyugu <1435221207@qq.com>
2025-12-11 22:35:24 +0800

    Add Pet class to pybind11 module and update test script for OOP functionality

--- test_for_pybind/src/test.cpp
+++ test_for_pybind/src/test.cpp
@@ -5,7 +5,25 @@ int add(int i, int j) {
     return i + j;
 }
 
+struct Pet {
+    Pet(const std::string &name) : name(name) { }
+    void setName(const std::string &name_) { name = name_; }
+    const std::string &getName() const { return name; }
+
+    std::string name;
+};
+
 PYBIND11_MODULE(pybind_test, m) {
     m.doc() = "pybind11 example plugin"; // optional module docstring
     m.def("add", &add, "A function that adds two numbers");
+    m.attr("the_answer") = 42;
+    py::object world = py::cast("World");
+    m.attr("what") = world;
+
+    // If you like OOP!
+    py::class_<Pet>(m, "Pet")
+        .def(py::init<const std::string &>())
+        .def("setName", &Pet::setName)
+        .def("getName", &Pet::getName);
 }
+
--- test_for_pybind/test.py
+++ test_for_pybind/test.py
@@ -1,3 +1,13 @@
 import build.pybind_test as pybind_test
 
 print(pybind_test.add(1,2))
+print(pybind_test.what)
+print(pybind_test.the_answer)
+
+# For OOP functionalities
+
+p = pybind_test.Pet("Molly")
+print(p)
+print("Pet's name:", p.getName())
+p.setName("Bella")
+print("Pet's new name:", p.getName())
